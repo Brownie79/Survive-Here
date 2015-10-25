b3b1208b9fb70682660401bebf9e0a0de0cbb81d
Brownie79 <1wolfer0@gmail.com>
2015-10-25 03:23:05 -0500

    commented code; begin map events

--- js/basicmap.js
+++ js/basicmap.js
@@ -11,6 +11,10 @@ var maptypes = platform.createDefaultLayers();
 var map = new H.Map(
 	document.getElementById('mapContainer'),
 	maptypes.satellite.map,{
-		zoom: 18, //about 2 blocks, takes ~30s actual walking time from end to end?
+		zoom: 19, //about 2 blocks, takes ~30s actual walking time from end to end?
 		center: { lng: -87.6252530, lat: 41.8261990 }
 });
+
+//https://developer.here.com/javascript-apis/documentation/v3/maps/topics/map-types.html
+//maptypes contains: normal, satellite, and terrain views, each of which have many subtypes
+//view reference link above for a table breakdown
